52eb48eaff27e51f98c41f12a888c8ca78493dfd
Ashton Williams <jynxdaddy+github@gmail.com>
2011-04-05 01:14:51 +1000

    Finished until Bukkit API updates.

--- src/main/java/com/jynxdaddy/wolfspawn/WolfListener.java
+++ src/main/java/com/jynxdaddy/wolfspawn/WolfListener.java
@@ -5,12 +5,14 @@ package com.jynxdaddy.wolfspawn;
 
 import java.util.logging.Logger;
 
+import org.bukkit.Location;
+import org.bukkit.entity.CreatureType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Wolf;
-import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityEvent;
 import org.bukkit.event.entity.EntityListener;
-import org.bukkit.event.entity.EntityTargetEvent;
 
 /**
  * @author Ashton
@@ -27,32 +29,21 @@ public class WolfListener extends EntityListener {
 	}
 
 	/* (non-Javadoc)
-	 * @see org.bukkit.event.entity.EntityListener#onCreatureSpawn(org.bukkit.event.entity.CreatureSpawnEvent)
-	 */
-	@Override
-	public void onCreatureSpawn(CreatureSpawnEvent event) {
-		if (isWolf(event)) {
-			log.info("onCreatureSpawn Wolf");
-		}
-	}
-
-	/* (non-Javadoc)
 	 * @see org.bukkit.event.entity.EntityListener#onEntityDeath(org.bukkit.event.entity.EntityDeathEvent)
 	 */
 	@Override
 	public void onEntityDeath(EntityDeathEvent event) {
 		if (isWolf(event)) {
 			log.info("onEntityDeath Wolf");
-		}
-	}
-
-	/* (non-Javadoc)
-	 * @see org.bukkit.event.entity.EntityListener#onEntityTarget(org.bukkit.event.entity.EntityTargetEvent)
-	 */
-	@Override
-	public void onEntityTarget(EntityTargetEvent event) {
-		if (isWolf(event)) {
-			log.info("onEntityTarget Wolf");
+			
+			
+			Wolf wolf = (Wolf) event.getEntity();
+			Player owner; //= wolf.getOwner();
+			
+			Location spawn = wolf.getWorld().getSpawnLocation();
+			LivingEntity newWolf = wolf.getWorld().spawnCreature(spawn, CreatureType.WOLF);
+			//newWolf.setOwner(owner);
+			
 		}
 	}
 	
